d476b0fb257fa2571b20b7f74de88d639160e4f8
mary <anacoin0014@gmail.com>
2022-10-04 17:27:29 +0330

    copy files add packages

---
+++ src/pages/about-us/AboutUs.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './AboutUs.style.scss'
+const AboutUs = () => {
+  return <>
+      <div className="about-container">
+          
+      </div>
+  </>;
+};
+
+export default AboutUs;
